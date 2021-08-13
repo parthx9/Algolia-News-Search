ccc707c39eecbc0c6d8f5fecf28dbb7179211c6e
Parth Arora <partharora.india@gmail.com>
2021-08-13 17:09:44 +0530

    news-card news-card-list components created

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,6 +1,7 @@
 const NewsCard = (prop) => {
   return (
     <div className="news-card">
+      <p className="subtext m-0">created by {prop.news.author} at {prop.news.created_at}</p>
       <h3 dangerouslySetInnerHTML={{ __html: prop.news._highlightResult.title.value }}></h3>
     </div>
   )

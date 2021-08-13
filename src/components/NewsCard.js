f383881b844d72ee3c09a479ee2c3641ece4c35d
Parth Arora <partharora.india@gmail.com>
2021-08-13 19:32:43 +0530

    search completed | handled errors null cases

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,27 +1,41 @@
 import { convertDiffDates } from "../utils/helperFunctions"
 
 const NewsCard = (prop) => {
+
+  const getTitle = () => {
+    if (prop.news.story_title && prop.news._highlightResult.title?.value) {
+      return prop.news.title
+    }
+    return (prop.news._highlightResult.title?.value) ? (prop.news._highlightResult.title?.value) : (
+      prop.news.story_title
+    )
+  }
+
+  if (prop.news.story_title && prop.news._highlightResult.title?.value && prop.news.title) {
+    return
+  }
+
   return (
     <div className='row news-card'>
       <div className='col-1'>
         <div className='points'>
           <span className="fa fa-chevron-up"></span>
-          {prop.news.points}
+          {prop.news.points ? prop.news.points : 0}
         </div>
       </div>
       <div className='col-11'>
         <div className="news-info">
-          <h3 dangerouslySetInnerHTML={{ __html: prop.news._highlightResult.title.value }}></h3>
+          <h3 dangerouslySetInnerHTML={{ __html: getTitle() }}></h3>
           <p className="subtext m-0">created by {prop.news.author}, {convertDiffDates(prop.news.created_at)}</p>
         </div>
         <div className='bottom-bar'>
           <div className="item">
             <span className="far fa-comment"></span>
-            <span>{prop.news.num_comments}</span>
+            <span>{prop.news.num_comments ? prop.news.num_comments : 0}</span>
           </div>
           <div className="item">
             <span className="fa fa-link"></span>
-            <a href={prop.news.url} target="_blank" rel="noreferrer" >View Source</a>
+            <a href={prop.news.url ? prop.news.url : prop.news.story_url} target="_blank" rel="noreferrer" >View Source</a>
           </div>
         </div>
       </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -8,11 +8,68 @@ const Home = () => {
 
   const [searchTerm, setSearchTerm] = useState("")
   const [searchResults, setSearchResults] = useState([])
+  const [status, setStatus] = useState('loaded')
 
-  useEffect(() => {
-    searchQuery('test')
-      .then(res => setSearchResults(res.hits))
-  }, [])
+  const renderTry = () => {
+
+    const samples = ['India', 'Bitcoin', 'Tesla']
+
+    return samples.map(i => {
+      return (
+        <span onClick={() => setSearchTerm(i)} className='query'>{i}</span>
+      )
+    })
+  }
+
+  const getResults = () => {
+    setStatus('loading')
+    searchQuery(searchTerm)
+      .then(res => {
+        if (res.hits.length === 0) {
+          setStatus('no-results')
+          setSearchResults([])
+        }
+        setSearchResults(res.hits)
+        setStatus('none')
+      })
+      .catch(err => {
+        setStatus('no-results')
+      })
+  }
+
+  const checkStatus = () => {
+    if (status === 'loaded') {
+      return (
+        <div className="search-something">
+          <span className="fa subtext fa-search"></span>
+          <h4 className="subtext">Your search results will appear here.</h4>
+        </div>
+      )
+    }
+
+    if (status === 'loading') {
+      return (
+        <div className="loading">
+          <div className='spinner'>
+          </div>
+          <h4 className="subtext">Loading your results...</h4>
+        </div>
+      )
+    }
+
+    if (status === 'no-results') {
+      return (
+        <div>
+          <img className="nothing-img" src={nothing} />
+          <p className="subtext">Nothing Found</p>
+        </div>
+      )
+    }
+
+    return (
+      <NewsCardList results={searchResults} />
+    )
+  }
 
   return (
     <div>
@@ -21,18 +78,21 @@ const Home = () => {
         <div className='container'>
           <div className='search-area'>
             <div className='search-bar'>
-              <input onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search Algolia...' />
+              <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search Algolia...' />
             </div>
-            <div className='search-btn'>
+            <div onClick={() => getResults()} className='search-btn'>
               <button>
                 Search
               </button>
             </div>
           </div>
+          <div className="try-area">
+            <div className='try-queries'>
+              <span className="pe-3">Try these: </span> {renderTry()}
+            </div>
+          </div>
           <div className='results-area'>
-            <NewsCardList results={searchResults} />
-            {/* <img className="nothing-img" src={nothing} alt="nothing-found" />
-            <h3 className="subtext mt-3">No results found, please try searching something else.</h3> */}
+            {checkStatus()}
           </div>
         </div>
       </div>

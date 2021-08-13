fe529ab25f0773c028c9ee4d857972a4d5b3b0c5
Parth Arora <partharora.india@gmail.com>
2021-08-14 00:12:29 +0530

    added home link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import { useHistory } from 'react-router'
 import logo from '../assets/imgs/logo.svg'
 const Header = () => {
+  const history = useHistory()
   return (
     <div className='header'>
-      <div className='logo'>
+      <div onClick={() => history.push('/')} className='logo'>
         <img src={logo} width="150px" alt="algolia" />
       </div>
     </div>
